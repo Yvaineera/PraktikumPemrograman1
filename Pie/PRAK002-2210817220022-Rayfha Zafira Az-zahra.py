07824f8c912f7aa3efe3a9853a6674a04ab1d508
Rayfha Zafira <rayfhazfr@gmail.com>
2022-10-04 13:56:28 +0800

    first commit

---
+++ Pie/PRAK002-2210817220022-Rayfha Zafira Az-zahra.py
@@ -0,0 +1,3 @@
+print("Selamat Pagi, Rayfha Zafira Az-zahra")
+print("Selamat Siang, Rayfha Zafira Az-zahra")
+print("Selamat Malam, Rayfha Zafira Az-zahra")
---
+++ Pie/PRAK003-2210817220022-Rayfha Zafira Az-zahra.py
@@ -0,0 +1,1 @@
+print("Andi berkata \"Saya pasti bisa\"")
