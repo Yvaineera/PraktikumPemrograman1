b48aaea61e8436428d9203da69f95820721b40a6
Rayfha Zafira <rayfhazfr@gmail.com>
2022-10-04 14:53:02 +0800

    first commit

---
+++ See/PRAK002-2210817220022-RayfhaZafiraAz-zahra.c.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Selamat Pagi, Rayfha Zafira Az-zahra\n");
+    printf("Selamat Siang, Rayfha Zafira Az-zahra\n");
+    printf("Selamat Malam, Rayfha Zafira Az-zahra");
+    return 0;
+}
---
+++ See/PRAK004-2210817220022-RayfhaZafiraAz-zahra.c.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("##############################\n");
+    printf("#                            #\n");
+    printf("#   Rayfha Zafira Az-zahra   #\n");
+    printf("#       2210817220022        #\n");
+    printf("#                            #\n");
+    printf("##############################\n");
+    return 0;
+}
